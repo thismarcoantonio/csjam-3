2acad672602fa410382b07375cfc3ca033c49a29
thismarcoantonio <thismarcoantonio@gmail.com>
2020-01-30 11:32:17 -0300

    Add enemy logic and add it portal spawner

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        this.HandleMovement(movement);
+    }
+
+    private void HandleMovement(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * movementSpeed * Time.deltaTime));
+    }
+}
