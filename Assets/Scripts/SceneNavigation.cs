ad33485fa8da228dc1fae15999959e9d4c3c2ab7
thismarcoantonio <thismarcoantonio@gmail.com>
2020-02-01 18:25:19 -0300

    Add main menu

---
+++ Assets/Scripts/SceneNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+    public GameObject credits;
+
+    public void NavigateToScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToggleCredits()
+    {
+        credits.SetActive(!credits.activeSelf);
+    }
+}
